d512a70c6a310e10e9a1e799f022f1bb651b4d7e
NizamChy <nizam.chy.cs@gmail.com>
2023-08-06 20:02:35 +0600

    Added all core concepts of function and objects

---
+++ 32-switch.js
@@ -0,0 +1,29 @@
+// switch
+// switch is faster than if-else
+// switch checks exact the given value
+// where if-else check every condition
+const color = "blue";
+switch (color) {
+  case "green":
+    console.log("The color is green");
+    break;
+  case "blue":
+    console.log("The color is blue");
+    break;
+  case "red":
+    console.log("The color is red");
+    break;
+  default:
+    console.log("I dont know this color");
+}
+
+// const color = "red";
+// if (color === "green") {
+//   console.log("The color is green");
+// } else if (color === "blue") {
+//   console.log("The color is blue");
+// } else if (color === "red") {
+//   console.log("The color is red");
+// } else {
+//   console.log("I dont know the color");
+// }
